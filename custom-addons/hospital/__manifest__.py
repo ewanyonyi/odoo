aa1ed17fdb24899de33f8e2129c33bce889b57e0
Emanuel Dickson Wanyonyi <ewanyonyi@kenet.or.ke>
2023-05-20 21:35:13 +0300

    Added custom module

---
+++ custom-addons/hospital/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Emanuel Wanyonyi',
+    # 'sequence': -100, # The module will appear first in the list
+    'summary': 'This is a hospital management module',
+    'description': 'This is a hospital management module',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    # 'application': True, #If the module is standalone
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'license': 'LGPL-3',
+} 
